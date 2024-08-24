fa08a1e63a6dd53ac4467a350af6d39d361d919d
r-krxsshna <ridhulkrishna2002@gmail.com>
2024-08-24 16:52:19 +0530

    Add files via upload

---
+++ lib/core/consts/api_key.dart
@@ -0,0 +1,8 @@
+/// Stores an API key
+class ApiKey {
+  static const _apiKey = '52f53f78f48a430d8e115b4ce1409308';
+
+  /// Returns an API key for accessing abstractapi's endpoint
+  static getKey() => _apiKey;
+}
+//52f53f78f48a430d8e115b4ce1409308
